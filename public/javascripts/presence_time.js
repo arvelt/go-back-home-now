cc72fd3ef80808f7c27d2399dc4928b707698c0b
Keisuke Ota <arvelt.ko@gmail.com>
2012-02-22 00:03:34 +0900

    Revert "bug fix" TO User ServerTime This reverts commit 8bad3a4b459726713a3b75c145a38e74cb3bf6bf.
    Conflicts:
    
    	app/controllers/user_controller.rb
    	app/controllers/user_controller.rb~

--- public/javascripts/presence_time.js
+++ public/javascripts/presence_time.js
@@ -1,57 +1,16 @@
 function jikoku() {
-    d = getNowDate();
-    t = getNowTime();
-    document.getElementById('presence_time').innerHTML = d + "　" + t;
-    window.setTimeout(jikoku, 970);
-}
-
-function getPresentTime( mydate , mytime ){
-    //alert( pressedName.value );
-    d = getNowDate();
-    d = replaceAll(d,"/","");
-    t = getNowhhmm();
-    //alert(d+t);
-    
-    // 日付をYYYYMMDD、時刻をhhmmとしてポスト
-    mydate.value = d;
-    mytime.value = t;
-}
-
-function getNowDate(){
     datetime = new Date();
     y = datetime.getFullYear();
     M = datetime.getMonth() + 1;
     d = datetime.getDate();
-
-    //"yyyy/MM/dd"に整形
-    now = y +"/"+ ("0"+M).slice(-2) +"/"+ ("0"+d).slice(-2) ;
-    return now ;
-}
-
-function getNowTime(){
-
-    datetime = new Date();
     h = datetime.getHours();
     m = datetime.getMinutes();
     s = datetime.getSeconds();
 
-    //"hh:mm:ss"に整形
-    now = ("0"+h).slice(-2) +":"+ ("0"+m).slice(-2) +":"+ ("0"+s).slice(-2);
-    return now ;
+    //"yyyy/MM/dd hh:mm:ss"に整形
+    now = y + "/" + ("0"+M).slice(-2) + "/" + ("0"+d).slice(-2) + "　" + 
+           ("0"+h).slice(-2) + ":" + ("0"+m).slice(-2) + ":" + ("0"+s).slice(-2);
+
+    document.getElementById('presence_time').innerHTML = now;
+    window.setTimeout("jikoku()", 1000);
 }
-
-function getNowhhmm(){
-
-    datetime = new Date();
-    h = datetime.getHours();
-    m = datetime.getMinutes();
-
-    //"hhmm"に整形
-    now = ("0"+h).slice(-2) + ("0"+m).slice(-2) ;
-    return now ;
-}
-
-function replaceAll(expression, org, dest){ 
-    return expression.split(org).join(dest);
-}
-
