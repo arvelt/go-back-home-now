5eed74395aff39540144ef2721ea538a7653cd31
Keisuke Ota <arvelt.ko@gmail.com>
2012-02-21 23:57:13 +0900

    Revert "Revert "add debug msg"" RevertVersioni is diffrent This reverts commit 5b646de8b89d974936865808a4dff7fa2dda430c.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -32,12 +32,16 @@ class LoginController < ApplicationController
 	session[:secret_key] = data['credentials']['secret']
     
     #すでにあるときは登録せずマイページへ
-    unless User.where(:user_id => user.user_id) == nil
+    data = User.where(:user_id => user.user_id).first
+    p data
+    unless data == nil
+      puts 'User data is already exists in t'
       redirect_to :controller => 'user' , :action => :index
     else  
 
         #ユーザー情報を保存して個人ページへ
         if user.save
+          puts 'Userdata is saved in t. user_id is '+user.user_id.to_s
           redirect_to :controller => 'user' , :action => :index
         else
           flash[:notice] = "User save is failed"
@@ -66,13 +70,13 @@ class LoginController < ApplicationController
     data = User.where(:user_id => user.user_id).first
     p data
     unless data == nil
-      puts 'User data is already exists'
+      puts 'User data is already exists in g'
       redirect_to :controller => 'user' , :action => :index
     else
 
         #ユーザー情報を保存して個人ページへ
         if user.save
-          puts 'Userdata is saved. user_id is '+user.user_id.to_s
+          puts 'Userdata is saved in g. user_id is '+user.user_id.to_s
           redirect_to :controller => 'user' , :action => :index
         else
           flash[:notice] = "User save is failed"
