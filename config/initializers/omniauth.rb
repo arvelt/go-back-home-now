ffe446f5982fd3050ed93d05c6ee0fc0b0db7b12
Keisuke Ota <arvelt.ko@gmail.com>
2012-09-15 23:49:28 +0900

    modity to get id from env_var

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :twitter, 'aLLC79cZvRHKe8jawQaGA', 'CMQp4Azbz1ad8bMcVqwQF0kmEE4MixkQpciqPmDc'
-  provider :twitter, 
-    File.read("config/initializers/twitter_id",:encoding=>Encoding::UTF_8), 
-    File.read("config/initializers/twitter_secret",:encoding=>Encoding::UTF_8)
-#  provider :google_oauth2, '40217307146.apps.googleusercontent.com' , 'AkFXU4w7dWWbmth-Be_DeFcX'
-  provider :google_oauth2, 
-    File.read("config/initializers/google_id",:encoding=>Encoding::UTF_8) , 
-    File.read("config/initializers/google_secret",:encoding=>Encoding::UTF_8)
+  provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET']
 end
 
 if Rails.env == "development"
